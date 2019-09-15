5d2a6a9af7582870926d64f03b0253d19297a7d0
Johan Wirén <johanwiren1@gmail.com>
2019-09-15 17:08:59 +0200

    card routes to friend details

--- frontend/src/components/FriendCard.js
+++ frontend/src/components/FriendCard.js
@@ -1,23 +1,28 @@
 import React from "react";
+import Clock from "./Clock";
+import { Route } from "react-router-dom";
 
 const FriendCard = props => {
-  function listKittens(kittens) {
-    return kittens.map((kitten, i) => (
-      <div key={kitten.name + i}>
-        <p>Name: {kitten.name}</p>
-        <p>Age: {kitten.age}</p>
-      </div>
-    ));
+  function getLocaleTime() {
+    let timeOffset = parseInt(props.timezone.substr(-2));
+    return <Clock {...{ timeOffset: timeOffset }} />;
   }
 
   return (
-    <div className="card">
-      <div className="card-content">
-        <span className="card-title">{props.name}</span>
-        <p>Age: {props.age}</p>
-        {listKittens(props.kittens)}
-      </div>
-    </div>
+    <Route
+      render={({ history }) => (
+        <div
+          className="card waves-effect light-waves"
+          onClick={() => history.push("/friend/" + props._id)}
+        >
+          <div className="card-content">
+            <span className="card-title">{props.name}</span>
+            <p>Local time</p>
+            {getLocaleTime()}
+          </div>
+        </div>
+      )}
+    />
   );
 };
 
--- frontend/src/pages/FriendDetails.js
+++ frontend/src/pages/FriendDetails.js
@@ -1,27 +1,48 @@
 import React, { Component } from "react";
-import FriendCard from '../components/FriendCard'
+import FriendCard from "../components/FriendCard";
 import Person from "../models/Person";
 
 export default class FriendDetails extends Component {
   state = {
-    friend: { kittens: [] }
+    friend: {
+      phoneNumbers: [],
+      mailAddresses: []
+    }
   };
 
   async componentDidMount() {
-    let friend = await Person.findOne("5d75f9ab9f405453784fb3c8");
+    let friend = await Person.findOne(this.props.match.params.id);
     console.log(friend);
     this.setState({ friend });
   }
 
+  listEmails(emails) {
+    return emails.map((email, i) => (
+      <div key={email + i}>
+        <p>{email}</p>
+      </div>
+    ));
+  }
 
+  listPhoneNumbers(phoneNumbers) {
+    return phoneNumbers.map((number, i) => (
+      <div key={number + i}>
+        <p>{number}</p>
+      </div>
+    ));
+  }
 
   render() {
+    let f = this.state.friend;
     return (
       <div>
         <h1>The friend page!</h1>
         <p>id: {this.props.match.params.id}</p>
-
-        <FriendCard {...this.state.friend} />
+        <p>Name: {f.name}</p>
+        <p>Phonenumbers</p>
+        {this.listPhoneNumbers(f.phoneNumbers)}
+        <p>Emails</p>
+        {this.listEmails(f.mailAddresses)}
       </div>
     );
   }
