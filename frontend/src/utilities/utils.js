0825e781866782f58cce543f44bb42fb298d6065
Johan Wirén <johanwiren1@gmail.com>
2019-10-01 11:33:57 +0200

    fixed validation

--- frontend/src/utilities/utils.js
+++ frontend/src/utilities/utils.js
@@ -24,20 +24,23 @@ export function availabilityIcon(friend) {
 
   return (isWorking ? <i className="material-icons right">emoji_transportation</i> 
         : isSleeping ? <i className="material-icons right">snooze</i> 
-        : <i className="material-icons right">mood</i>)
+        : <i className="material-icons right">mood</i>);
 } 
 
 export function validateForm(s) {
-  const valName = !(!s.name.trim() && document.querySelector('#friend-name').classList.add("validate-error")) 
-  const valCity = !(!s.city.trim() && document.querySelector('#friend-city').classList.add("validate-error"))
-
+  !s.name.trim().length && document.querySelector('#friend-name').classList.add("validate-error");
+  const valName = s.name.trim().length > 0;
+  
+  !s.city.trim().length && document.querySelector('#friend-city').classList.add("validate-error");
+  const valCity = s.city.trim().length > 0;
+  
   const valPhone = (!s.phoneNumbers.map((m, i) => {
     if(m.length < 10 || m.length > 16) {
       document.querySelector('.friend-phone-' + i).classList.add("validate-error");
       return i;
     } 
     return undefined;
-  }).filter(m => m !== undefined).length > 0)
+  }).filter(m => m !== undefined).length > 0);
 
   const valEmail = (!s.mailAddresses.map((m, i) => {
     if(!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(m)) {
@@ -45,7 +48,7 @@ export function validateForm(s) {
       return i;
     } 
     return undefined;
-  }).filter(m => m !== undefined).length > 0)
+  }).filter(m => m !== undefined).length > 0);
 
   return valName && valCity && valPhone && valEmail;
 }
